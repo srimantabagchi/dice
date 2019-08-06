d9ea859928fb470b1fbcc1e7f2e1d44b7075e360
Srimanta Bagchi <srimantabagchi@gmail.com>
2019-08-06 09:24:59 -0400

    Write die component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Die from './Die';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Die face='five' />
+    </div>
+  );
+}
+
+export default App;
